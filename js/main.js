ea8998aa78b44bba126b5edaf8650cc5b7e4dff9
rinox3005 <peppe.rinox@gmail.com>
2024-05-28 17:41:39 +0200

    [UPD] added generate button, bonus done

--- js/main.js
+++ js/main.js
@@ -9,13 +9,15 @@ createApp({
       emails: [],
       length: 10,
       url: "https://flynn.boolean.careers/exercises/api/random/mail",
+      message: "Generate",
     };
   },
-  mounted() {
-    this.getEmails();
-  },
+  //   mounted() {
+  //     this.getEmails();
+  //   },
   methods: {
     getEmails() {
+      this.message = "Wait...";
       for (let i = 0; i < this.length; i++) {
         axios.get(this.url).then((response) => {
           this.emails.push(response.data.response);
