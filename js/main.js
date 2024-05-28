d3ff418564e50d88c69fe38068c2dd6376cb8a56
rinox3005 <peppe.rinox@gmail.com>
2024-05-28 17:23:25 +0200

    [UPD] used axios to make api call and pushed the result in array to show it on page

--- js/main.js
+++ js/main.js
@@ -5,7 +5,22 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      title: "Prova Email API",
+      title: "Email API",
+      emails: [],
+      length: 10,
+      url: "https://flynn.boolean.careers/exercises/api/random/mail",
     };
+  },
+  mounted() {
+    this.getEmails();
+  },
+  methods: {
+    getEmails() {
+      for (let i = 0; i < this.length; i++) {
+        axios.get(this.url).then((response) => {
+          this.emails.push(response.data.response);
+        });
+      }
+    },
   },
 }).mount("#app");
